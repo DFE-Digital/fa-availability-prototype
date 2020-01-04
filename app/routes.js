829ff3a18d7f56488fce844067d669760821cc77
alexbowen <alexbowen@alexs-mbp.home>
2020-01-04 13:53:13 +0000

    add foster parent data

--- app/routes.js
+++ app/routes.js
@@ -3,20 +3,22 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+const fosterParents = require('./data/foster-parents')
+
 router.get('/search-confirmation', (req, res) => {
       res.render('availability/search-confirmation')
   })
 
   router.get('/shortlist', (req, res) => {
-    res.render('availability/shortlist')
+    res.render('availability/shortlist', { fosterParents })
 })
 
 router.get('/my-list', (req, res) => {
-  res.render('availability/my-list')
+  res.render('availability/my-list', { fosterParents })
 })
 
 router.get('/foster-parent-profile', (req, res) => {
-  res.render('availability/foster-parent-profile')
+  res.render('availability/foster-parent-profile', { parent: fosterParents.find(parent => parent.id == req.query.id)})
 })
 
 router.get('/child-profile', (req, res) => {

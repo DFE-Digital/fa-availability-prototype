4c8a5f2dc3791d4fc0eeb7738db83c3a69f83da6
alexbowen <alexbowen@alexs-mbp.home>
2020-01-04 08:39:21 +0000

    add initial page templates

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,24 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.get('/search-confirmation', (req, res) => {
+      res.render('availability/search-confirmation')
+  })
+
+  router.get('/shortlist', (req, res) => {
+    res.render('availability/shortlist')
+})
+
+router.get('/my-list', (req, res) => {
+  res.render('availability/my-list')
+})
+
+router.get('/foster-parent-profile', (req, res) => {
+  res.render('availability/foster-parent-profile')
+})
+
+router.get('/child-profile', (req, res) => {
+  res.render('availability/child-profile')
+})
+
 module.exports = router
